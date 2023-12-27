4ab39bd74538e3065aa9df0e2a6433838420f851
Srishti  Jha <72248660+SriCoD@users.noreply.github.com>
2023-12-27 13:51:24 +0530

    Update LiveChat.js

--- src/components/LiveChat.js
+++ src/components/LiveChat.js
@@ -45,7 +45,7 @@ const LiveChat = () => {
 
           dispatch(
             addMessage({
-              name: "Akshay Saini",
+              name: "Srishti Jha",
               message: liveMessage,
             })
           );
